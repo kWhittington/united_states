f933f5ade080f44eff01504f0a781f4ab3749420
Kyle Whittington <kyle.thomas.whittington@gmail.com>
2017-02-08 15:16:58 -0600

    Add UnitedStates.all
    `UnitedStates.all` will return all 50 U.S. State designations.

--- lib/united_states.rb
+++ lib/united_states.rb
@@ -1,7 +1,120 @@
 # frozen_string_literal: true
 require 'united_states/version'
-require 'united_states/state/name'
+require 'united_states/state/designation'
 
+# rubocop: disable Metrics/ModuleLength
+#
 # Top-level namespace for this gem.
 module UnitedStates
+  # rubocop: disable Metrics/AbcSize
+  # rubocop: disable Metrics/MethodLength
+  # @return [Array<UnitedStates::State::Designation>]
+  #  a collection of all U.S. State Designations.
+  def self.all
+    [
+      UnitedStates::State::Designation.new(
+        name: 'Alabama', abbreviation: 'AL'),
+      UnitedStates::State::Designation.new(
+        name: 'Alaska', abbreviation: 'AK'),
+      UnitedStates::State::Designation.new(
+        name: 'Arizona', abbreviation: 'AZ'),
+      UnitedStates::State::Designation.new(
+        name: 'Arkansas', abbreviation: 'AR'),
+      UnitedStates::State::Designation.new(
+        name: 'California', abbreviation: 'CA'),
+      UnitedStates::State::Designation.new(
+        name: 'Colorado', abbreviation: 'CO'),
+      UnitedStates::State::Designation.new(
+        name: 'Connecticut', abbreviation: 'CT'),
+      UnitedStates::State::Designation.new(
+        name: 'Delaware', abbreviation: 'DE'),
+      UnitedStates::State::Designation.new(
+        name: 'Florida', abbreviation: 'FL'),
+      UnitedStates::State::Designation.new(
+        name: 'Georgia', abbreviation: 'GA'),
+      UnitedStates::State::Designation.new(
+        name: 'Hawaii', abbreviation: 'HI'),
+      UnitedStates::State::Designation.new(
+        name: 'Idaho', abbreviation: 'ID'),
+      UnitedStates::State::Designation.new(
+        name: 'Illinois', abbreviation: 'IL'),
+      UnitedStates::State::Designation.new(
+        name: 'Indiana', abbreviation: 'IN'),
+      UnitedStates::State::Designation.new(
+        name: 'Iowa', abbreviation: 'IA'),
+      UnitedStates::State::Designation.new(
+        name: 'Kansas', abbreviation: 'KS'),
+      UnitedStates::State::Designation.new(
+        name: 'Kentucky', abbreviation: 'KY'),
+      UnitedStates::State::Designation.new(
+        name: 'Louisiana', abbreviation: 'LA'),
+      UnitedStates::State::Designation.new(
+        name: 'Maine', abbreviation: 'ME'),
+      UnitedStates::State::Designation.new(
+        name: 'Maryland', abbreviation: 'MD'),
+      UnitedStates::State::Designation.new(
+        name: 'Massachusetts', abbreviation: 'MA'),
+      UnitedStates::State::Designation.new(
+        name: 'Michigan', abbreviation: 'MI'),
+      UnitedStates::State::Designation.new(
+        name: 'Minnesota', abbreviation: 'MN'),
+      UnitedStates::State::Designation.new(
+        name: 'Mississippi', abbreviation: 'MS'),
+      UnitedStates::State::Designation.new(
+        name: 'Missouri', abbreviation: 'MO'),
+      UnitedStates::State::Designation.new(
+        name: 'Montana', abbreviation: 'MT'),
+      UnitedStates::State::Designation.new(
+        name: 'Nebraska', abbreviation: 'NE'),
+      UnitedStates::State::Designation.new(
+        name: 'Nevada', abbreviation: 'NV'),
+      UnitedStates::State::Designation.new(
+        name: 'New Hampshire', abbreviation: 'NH'),
+      UnitedStates::State::Designation.new(
+        name: 'New Jersey', abbreviation: 'NJ'),
+      UnitedStates::State::Designation.new(
+        name: 'New Mexico', abbreviation: 'NM'),
+      UnitedStates::State::Designation.new(
+        name: 'New York', abbreviation: 'NY'),
+      UnitedStates::State::Designation.new(
+        name: 'North Carolina', abbreviation: 'NC'),
+      UnitedStates::State::Designation.new(
+        name: 'North Dakota', abbreviation: 'ND'),
+      UnitedStates::State::Designation.new(
+        name: 'Ohio', abbreviation: 'OH'),
+      UnitedStates::State::Designation.new(
+        name: 'Oklahoma', abbreviation: 'OK'),
+      UnitedStates::State::Designation.new(
+        name: 'Oregon', abbreviation: 'OR'),
+      UnitedStates::State::Designation.new(
+        name: 'Pennsylvania', abbreviation: 'PA'),
+      UnitedStates::State::Designation.new(
+        name: 'Rhode Island', abbreviation: 'RI'),
+      UnitedStates::State::Designation.new(
+        name: 'South Carolina', abbreviation: 'SC'),
+      UnitedStates::State::Designation.new(
+        name: 'South Dakota', abbreviation: 'SD'),
+      UnitedStates::State::Designation.new(
+        name: 'Tennessee', abbreviation: 'TN'),
+      UnitedStates::State::Designation.new(
+        name: 'Texas', abbreviation: 'TX'),
+      UnitedStates::State::Designation.new(
+        name: 'Utah', abbreviation: 'UT'),
+      UnitedStates::State::Designation.new(
+        name: 'Vermont', abbreviation: 'VT'),
+      UnitedStates::State::Designation.new(
+        name: 'Virginia', abbreviation: 'VA'),
+      UnitedStates::State::Designation.new(
+        name: 'Washington', abbreviation: 'WA'),
+      UnitedStates::State::Designation.new(
+        name: 'West Virginia', abbreviation: 'WV'),
+      UnitedStates::State::Designation.new(
+        name: 'Wisconsin', abbreviation: 'WI'),
+      UnitedStates::State::Designation.new(
+        name: 'Wyoming', abbreviation: 'WY')
+    ]
+  end
+  # rubocop: enable Metrics/AbcSize
+  # rubocop: enable Metrics/MethodLength
 end
+# rubocop: enable Metrics/ModuleLength
--- spec/united_states_spec.rb
+++ spec/united_states_spec.rb
@@ -10,4 +10,118 @@ RSpec.describe UnitedStates do
   it 'loads UnitedStates::State::Name' do
     expect(UnitedStates::State::Name).not_to be_nil
   end
+
+  describe '.all' do
+    subject(:all) { described_class.all }
+
+    it 'contains 50 designations' do
+      expect(all.count).to eq(50)
+    end
+
+    # rubocop: disable RSpec/ExampleLength
+    it 'contains each state designations' do
+      is_expected.to contain_exactly(
+        UnitedStates::State::Designation.new(
+          name: 'alabama', abbreviation: 'al'),
+        UnitedStates::State::Designation.new(
+          name: 'alaska', abbreviation: 'ak'),
+        UnitedStates::State::Designation.new(
+          name: 'arizona', abbreviation: 'az'),
+        UnitedStates::State::Designation.new(
+          name: 'arkansas', abbreviation: 'ar'),
+        UnitedStates::State::Designation.new(
+          name: 'california', abbreviation: 'ca'),
+        UnitedStates::State::Designation.new(
+          name: 'colorado', abbreviation: 'co'),
+        UnitedStates::State::Designation.new(
+          name: 'connecticut', abbreviation: 'ct'),
+        UnitedStates::State::Designation.new(
+          name: 'delaware', abbreviation: 'de'),
+        UnitedStates::State::Designation.new(
+          name: 'florida', abbreviation: 'fl'),
+        UnitedStates::State::Designation.new(
+          name: 'georgia', abbreviation: 'ga'),
+        UnitedStates::State::Designation.new(
+          name: 'hawaii', abbreviation: 'hi'),
+        UnitedStates::State::Designation.new(
+          name: 'idaho', abbreviation: 'id'),
+        UnitedStates::State::Designation.new(
+          name: 'illinois', abbreviation: 'il'),
+        UnitedStates::State::Designation.new(
+          name: 'indiana', abbreviation: 'in'),
+        UnitedStates::State::Designation.new(
+          name: 'iowa', abbreviation: 'ia'),
+        UnitedStates::State::Designation.new(
+          name: 'kansas', abbreviation: 'ks'),
+        UnitedStates::State::Designation.new(
+          name: 'kentucky', abbreviation: 'ky'),
+        UnitedStates::State::Designation.new(
+          name: 'louisiana', abbreviation: 'la'),
+        UnitedStates::State::Designation.new(
+          name: 'maine', abbreviation: 'me'),
+        UnitedStates::State::Designation.new(
+          name: 'maryland', abbreviation: 'md'),
+        UnitedStates::State::Designation.new(
+          name: 'massachusetts', abbreviation: 'ma'),
+        UnitedStates::State::Designation.new(
+          name: 'michigan', abbreviation: 'mi'),
+        UnitedStates::State::Designation.new(
+          name: 'minnesota', abbreviation: 'mn'),
+        UnitedStates::State::Designation.new(
+          name: 'mississippi', abbreviation: 'ms'),
+        UnitedStates::State::Designation.new(
+          name: 'missouri', abbreviation: 'mo'),
+        UnitedStates::State::Designation.new(
+          name: 'montana', abbreviation: 'mt'),
+        UnitedStates::State::Designation.new(
+          name: 'nebraska', abbreviation: 'ne'),
+        UnitedStates::State::Designation.new(
+          name: 'nevada', abbreviation: 'nv'),
+        UnitedStates::State::Designation.new(
+          name: 'new hampshire', abbreviation: 'nh'),
+        UnitedStates::State::Designation.new(
+          name: 'new jersey', abbreviation: 'nj'),
+        UnitedStates::State::Designation.new(
+          name: 'new mexico', abbreviation: 'nm'),
+        UnitedStates::State::Designation.new(
+          name: 'new york', abbreviation: 'ny'),
+        UnitedStates::State::Designation.new(
+          name: 'north carolina', abbreviation: 'nc'),
+        UnitedStates::State::Designation.new(
+          name: 'north dakota', abbreviation: 'nd'),
+        UnitedStates::State::Designation.new(
+          name: 'ohio', abbreviation: 'oh'),
+        UnitedStates::State::Designation.new(
+          name: 'oklahoma', abbreviation: 'ok'),
+        UnitedStates::State::Designation.new(
+          name: 'oregon', abbreviation: 'or'),
+        UnitedStates::State::Designation.new(
+          name: 'pennsylvania', abbreviation: 'pa'),
+        UnitedStates::State::Designation.new(
+          name: 'rhode island', abbreviation: 'ri'),
+        UnitedStates::State::Designation.new(
+          name: 'south carolina', abbreviation: 'sc'),
+        UnitedStates::State::Designation.new(
+          name: 'south dakota', abbreviation: 'sd'),
+        UnitedStates::State::Designation.new(
+          name: 'tennessee', abbreviation: 'tn'),
+        UnitedStates::State::Designation.new(
+          name: 'texas', abbreviation: 'tx'),
+        UnitedStates::State::Designation.new(
+          name: 'utah', abbreviation: 'ut'),
+        UnitedStates::State::Designation.new(
+          name: 'vermont', abbreviation: 'vt'),
+        UnitedStates::State::Designation.new(
+          name: 'virginia', abbreviation: 'va'),
+        UnitedStates::State::Designation.new(
+          name: 'washington', abbreviation: 'wa'),
+        UnitedStates::State::Designation.new(
+          name: 'west virginia', abbreviation: 'wv'),
+        UnitedStates::State::Designation.new(
+          name: 'wisconsin', abbreviation: 'wi'),
+        UnitedStates::State::Designation.new(
+          name: 'wyoming', abbreviation: 'wy'))
+    end
+    # rubocop: enable RSpec/ExampleLength
+  end
 end
